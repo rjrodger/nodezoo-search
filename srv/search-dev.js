567970a6846a2d3a258170c93f95b855953ff1a2
Cristian Kiss <cristian.kiss@nearform.com>
2016-01-20 15:21:02 +0200

    add actions to be called from nodezoo-web

--- srv/search-dev.js
+++ srv/search-dev.js
@@ -3,4 +3,15 @@ var STATS = process.env.STATS || 'localhost'
 
 require('seneca')()
   .use('../search.js',{elastic:{host:ELASTIC}})
+  
+  .add('role:search,cmd:insert',function(args,done){
+   done()
+   this.act('role:search,cmd:insert', {data: args.data})
+   })
+  .add('role:search,req:search',function(args,done){
+    done()
+    console.log('search')
+    this.act('role:search,cmd:search',{query:args.query})
+  })
+
   .use('mesh',{auto:true, pin:'role:search'})
