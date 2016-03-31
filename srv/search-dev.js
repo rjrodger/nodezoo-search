d817feb13ffb25aba4946bbb15952ad57a110799
Matthew O'Connor <matthewocsecond@gmail.com>
2016-03-31 17:52:46 +0100

    added new source of truth for search

--- srv/search-dev.js
+++ srv/search-dev.js
@@ -1,17 +1,23 @@
 'use strict'
 
-var opts = {
-  search: {
-    elastic: {
-      host: process.env.ELASTIC || 'localhost'
-    }
-  },
-  mesh: {
-    auto: true,
-    pin: 'role:search, cmd:*'
-  }
-}
-
-require('seneca')()
-  .use('../search.js', opts.search)
-  .use('mesh', opts.mesh)
+ require('seneca')()
+ .use('../search.js', {search: {
+   elastic: {
+     host: process.env.ELASTIC || 'localhost'
+   }
+ }})
+ .add('role:info,info:updated', function (args, done) {
+   done()
+   this.act('role:search,cmd:insert', {name: args.name}, function (err, mod) {
+     if (err) {
+       return done(err);
+     }
+      return done();
+   })
+ })
+    
+ .use('mesh', {
+   auto: true,
+   pin: ['role:search', 'role:info,info:updated'],
+   model: 'publish'
+ })
